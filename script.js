a6d1044b71ffa7098773b1d5f9757d3578d91d26
CACLR <136112842+CACLR@users.noreply.github.com>
2024-02-26 05:45:57 +0200

    Updated Link

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('visit-link').addEventListener('click', function() {
-    window.location.href = 'https://wa.me/27837150580';
+    window.location.href = 'https://wa.me/27837150580?text=Hi%20I%20am%20interested%20in%20the%202022%20R-Line%20Tiguan';
 });
